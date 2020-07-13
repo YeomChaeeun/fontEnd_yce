dbb8a2598ff2c81e07a7c0b009c7cd9e4825e42d
YeomChaeEun <yce0222@gmail.com>
2020-07-13 11:48:09 +0900

    big image slide 구현

--- js/src/sub_01.js
+++ js/src/sub_01.js
@@ -8,40 +8,21 @@
   var ImgUl = imgPart.children('ul');
   var ImgLi = ImgUl.find('li');
   var nextBtn = ImgArea.find('button');
+  var ImgLen = ImgLi.length;
 
   var ImgBig = subBox.find('.img_big');
+  var BigNextBtn = ImgBig.find('.next');
   var CloseBtn = ImgBig.find('.close_btn');
   
   var check = true;
   var n = 0;
   var timed = 600;
   
-  // subBox 자동슬라이드
-    
-  // -------------------------------------------------
-  // img_big 파트 나타내기
-  ImgUl.on('click',function(e){
-    e.preventDefault();
-    ImgBig.animate({left:0});
-  })
-  CloseBtn.on('click',function(e){
-    e.preventDefault();
-    ImgBig.animate({left:'100%'});
-  })
+  // subBox 자동슬라이드 fadeIn, fadeOut
 
-  // ImgBig 내부 ul 복사해오기
-  var BigImgCont = ImgBig.find('.img_cont');
-  // var ImgLast = ImgLi.eq(-1).clone(true);
-  // ImgUl.prepend(ImgLast);
-  // ImgLi = ImgUl.find('li');
-  var ImgLen = ImgLi.length;
-  // console.log(ImgLen);
-  // ImgUl.css({width:(ImgLen*100)+'%'});
-  // ImgLi.css({width:(100/ImgLen)+'%'});
-  
   var FadeFn = function(n,k){
     if(k!==n){ 
-      ImgLi.eq(n).css({zIndex:6, display:'block'});
+      ImgLi.eq(n).css({zIndex:10, display:'block'});
       ImgLi.eq(k).fadeOut(timed,function(){
         ImgLi.eq(n).css({zIndex:10});
         ImgLi.eq(n).siblings('li').css({zIndex:0});
@@ -49,6 +30,7 @@
       });
     }
   }
+  ImgLi.eq(0).css({zIndex:6, display:'block'});
   nextBtn.on('click',function(e){
     e.preventDefault();
     var k = n;
@@ -56,7 +38,8 @@
 
     if(thisBtn === nextBtn[0] && check){
       check = false;
-      (n>=ImgLen)? n=0 : n+=1;
+      (n>=ImgLen-1)? n=0 : n+=1;
+      console.log(n,k);
       FadeFn(n,k);
     }
   });
@@ -68,13 +51,60 @@
       nextBtn.trigger('click');
     }, timed*4);
   };
-
   mvSlideGo();
-
   var ClearFn = function(){clearInterval(mvImg)};
   var GoFn = function(){mvSlideGo()};
-
   ImgUl.on({'mouseenter':ClearFn, 'mouseleave':GoFn});
+
+  // -------------------------------------------------------------------
+  // img_big 파트 나타내기
+
+  // ImgBig 내부 ul 복사해오기
+  var BigImgCont = ImgBig.find('.img_cont');
+  var ImgUlCopy = ImgUl.clone();
+  BigImgCont.prepend(ImgUlCopy);
+  var BigUl = BigImgCont.children('ul');
+  var BigLi = BigUl.find('li');
+  var BIgLast = BigLi.clone();
+
+  BigUl.prepend(BIgLast);
+  BigLi = BigUl.find('li');
+  var BigImgLen = BigLi.length;
+
+  var s = 0;
+
+  ImgUl.on('click',function(e){
+    e.preventDefault();
+    ImgBig.css({display:'block'});
+    ImgBig.animate({left:0});
+  });
+  CloseBtn.on('click',function(e){
+    e.preventDefault();
+    ImgBig.animate({left:'100%'},function(){
+      ImgBig.css({display:'none'});
+      BigUl.css({marginLeft:'0%'});
+      s=0;
+    });
+  });
+
+  BigUl.css({width:(BigImgLen*100)+'%'});
+  BigLi.css({width:(100/BigImgLen)+'%'});
+
+  // BigImg 내 이미지 슬라이드
+  BigNextBtn.on('click',function(e){
+    e.preventDefault();
+    check=false;
+    s++;
+    BigUl.animate({marginLeft:-s*100+'%'},function(){
+      if(s>=BigImgLen/2){
+        s=0;
+        BigUl.css({marginLeft:'0%'});
+      }
+      check=true;
+    });
+    check=true;
+  });
+
 
   
 })(jQuery);
